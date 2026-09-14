2c783baee8e559d0d24d4e54f00485f1d51298b3
AlezzzGutierrez <gianellaagutierrez28@gmail.com>
2026-09-13 21:19:30 -0300

    Gianella Gutierrez Corrijo comentario en archivo

--- src/services/ejercicio04service.js
+++ src/services/ejercicio04service.js
@@ -1,4 +1,4 @@
-// services/validaciones.js
+
 
 // Array con los números válidos
 const numerosValidos = ["0", "1", "3", "5"];
@@ -6,5 +6,5 @@ const numerosValidos = ["0", "1", "3", "5"];
 // Función flecha para validar
 const esNumeroValido = (num) => numerosValidos.includes(num);
 
-// Exportamos la función para usarla en otros archivos
+
 export { esNumeroValido };
